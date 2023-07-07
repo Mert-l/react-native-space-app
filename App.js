313b93db8fa47ec8707022eea73654ad1323fe42
unknown <marrta.lm@gmail.com>
2023-07-07 18:41:40 +0300

    dafreg

--- App.js
+++ App.js
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Video from 'react-native-video';
+import axios from 'axios';
+import * as $ from 'jquery';
+import video from './assets/Earth_Background.mp4';
+import { Video, ResizeMode } from 'expo-av'
 
 export default function App() {
+
+//   useEffect(() => {
+//     const fetchWeather = async() =>{    
+//       const res= await axios.get(`https://api.nasa.gov/techtransfer/patent/?engine&api_key=DEMO_KEY`)
+
+   
+//         console.log( 'smth transfer',  res);
+//         // cons}])
+//     }
+//     fetchWeather()
+//     }, []);
+
+//     useEffect(() => {
+//       const fetchWeather = async() =>{    
+//         const res= await axios.get( `https://api.nasa.gov/DONKI/GST?startDate=2023-05-01&endDate=2023-05-07&api_key=DEMO_KEY`)
+  
+     
+//           console.log( 'res from reover?',  res);
+//           // cons}])
+//       }
+//       fetchWeather()
+//       }, []);
+
+
+
+
+
+
+//     // const request = require('request');
+//     // var name = 'Neptune';
+//     // request.get({
+//     //   url: 'https://api.api-ninjas.com/v1/planets?name=' + name,
+//     //   headers: {
+//     //     'X-Api-Key': 'Y/KpXwENCt0MuooEtwdh0w==nKpoX10n25tmB1O4'
+//     //   },
+//     // }, function(error, response, body) {
+//     //   if(error) return console.error('Request failed:', error);
+//     //   else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+//     //   else console.log(body)
+//     // });
+
+
+
+// console.log('fvsfef')
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+
+    <Video
+          source={video}
+          rate={1.0}
+          volume={1.0}
+          muted={false}
+          resizeMode={"cover"}
+          repeat
+          style={styles.video}
+        />
+
+
+
     </View>
   );
 }
---
+++ screens.js/Home.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Video from 'react-native-video';
+import video from './assets/Earth_Background.mp4';
+import React, { useState } from 'react';
+
+export default function Home() {
+
+  const [isPlaying, setIsPlaying] = React.useState(false);  
+    const [isMuted, setIsMuted] = React.useState(true);  
+
+
+  return (
+    <View >
+     
+
+
+
+
+    </View>
+  );
+}
