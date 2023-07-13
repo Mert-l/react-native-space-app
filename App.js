ea30f26d7ef3477e4e7dcad7b878dc95d1db9e89
unknown <marrta.lm@gmail.com>
2023-07-13 18:16:14 +0300

    dafreg

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRoute } from '@react-navigation/native';
 // import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -15,14 +15,136 @@ import Another from  "./screens.js/Another.js"
 import Onemore from  "./screens.js/Onemore.js"
  import globe from  "./screens.js/Globe.js"
 //  import Another_try from  "./screens.js/Another_try"
-import Notifications from  "./screens.js/Notifications"
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 
 
 export default function App() {
 
 
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+
+
+
  const [route, setRoute] = useState('')
   const Drawer = createDrawerNavigator();
+
+
+
+
+  let registerForPushNotificationsAsync = async() => {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log('Your token is ', token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
+
+
+
+
+
+
+
+
+
+  let sendTokenToServer = async(token, userID)=>{
+    let url = `http://192.168.1.153:3030/gettoken`;
+
+    axios.post(url, {token,userID} )
+    .then((res)=>{
+      console.log(res.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+    .then(token => {
+      setExpoPushToken(token)
+      if(token){sendTokenToServer(token, 'someUsername')}
+    })
+
+
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+
+
+  async function sendPushNotification(expoPushToken) {
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'Original Title',
+      body: 'And here is the body!',
+      data: { someData: 'goes here' },
+    };
+  
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
 
 
   return (
@@ -40,9 +162,9 @@ export default function App() {
         <Drawer.Screen name="People in space" component={Other} />
         <Drawer.Screen name="Mars photo database" component={Try}   />
         <Drawer.Screen name="Space News" component={Another}   />
-        <Drawer.Screen name="upcoming launches" component={Onemore} />
+        <Drawer.Screen name="upcoming launches" component={Onemore}    send={sendPushNotification()} />
         {/* <Drawer.Screen name="Another_try" component={Another_try} /> */}
-        <Drawer.Screen name="nots" component={Notifications} />
+        
        
       </Drawer.Navigator>
 
@@ -51,7 +173,7 @@ export default function App() {
 
 
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
--- screens.js/Onemore.js
+++ screens.js/Onemore.js
@@ -2,10 +2,9 @@ import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, ScrollVi
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Linking } from 'react-native';
-import Notifications from  "./Notifications"
 
 
-export default function Onemore() {
+export default function Onemore(props) {
 
     const [launches, setLanuches] = useState(null);
     const arr = [];
@@ -51,19 +50,7 @@ export default function Onemore() {
 launches && organise();
   console.log( 'arrr:',  arr)
 
-  const send_notification = () => {
 
-      for (ele of launches){
-
-          if(true){
-            <Notifications/>
-          }
-
-
-      }
-
-
-  }
 
 
 // launches && console.log(launches)
